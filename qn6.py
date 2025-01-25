68e3c52c71a25862b06fe769b892586bf61ab8bc
Sandeep Harijan <158283217+gtmsandy@users.noreply.github.com>
2025-01-25 20:15:25 +0000

    updated

---
+++ qn6.py
@@ -0,0 +1,11 @@
+# Check if a password is "Weak", "Medium", or "Strong". Criteria: < 6 chars (Weak), 6-10 chars (Medium), >10 chars (Strong).
+
+password = input('Enter the password: ')
+length = len(password)
+
+if length<6:
+    print('Weak password')
+elif length<=10:
+    print('Medium password')
+else:
+    print('Strong password')
